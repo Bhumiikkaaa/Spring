38f265505b35fed3f31e4325053e221b19e8580f
Bhumiikkaaa <155503334+Bhumiikkaaa@users.noreply.github.com>
2024-02-16 22:53:42 +0530

    Add files via upload

---
+++ LearnLinkedHashSet.java
@@ -0,0 +1,29 @@
+import java.util.Set;
+import java.util.LinkedHashSet;
+
+public class LearnLinkedHashSet {
+    public static void main(String[] args){
+        Set<Integer> set = new LinkedHashSet<>();
+        set.add(45);
+        set.add(34);
+        set.add(56);
+        set.add(12);
+        set.add(58);
+        set.add(45); //Duplicate elements cannot be put in a set
+        System.out.println(set);
+        // Order is defined in a  linked hashset.
+        //Every element has a unique hash
+        set.remove(12);
+        System.out.println(set);
+        set.contains(34); //Returns boolean - true or false value
+        //Checks whether an element is present or not
+        System.out.println(set.isEmpty());
+        //Returns boolean value
+        System.out.println(set.size()); //Returns the no of elements in a set
+        set.clear();
+        System.out.println(set); 
+        System.out.println(set.isEmpty());
+    }
+    
+}
+
